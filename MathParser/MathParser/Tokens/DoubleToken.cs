d45be2a39a1caba92113706ec47a62369c5b260c
Igor Fedorov <ifedorov@smartru.com>
2015-09-28 11:53:25 +0300

    add MathParser project, add DoubleToken class, add unit test for DoubleToken

---
+++ MathParser/MathParser/Tokens/DoubleToken.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathParser.Tokens
+{
+    class DoubleToken
+    {
+        double value;
+
+        int endPosition;
+
+        protected bool CheckSymbol(char symbol)
+        {
+            return true;
+        }
+        public DoubleToken(string source, int position = 0)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            char symbol;
+            while (position < source.Length)
+            {
+                symbol = source[position];
+                if (!CheckSymbol(symbol)) break;
+
+                sb.Append(symbol);
+                position += 1;   
+            }
+
+            if (double.TryParse(sb.ToString(), out this.value))
+            {
+                endPosition = position;
+            }
+            else
+            {
+                if (position == source.Length)
+                    throw new ArgumentException(string.Format("invalide double: '{0}'", sb.ToString()));
+
+                throw new ArgumentException(string.Format("unexpected symbol: '{0}'", source[position]));
+            }
+
+        }
+
+        public double Value { get { return value; } }
+    }
+}
---
+++ MathParser/UnitTest_MathParser/UnitTest_DoubleToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MathParser.Tokens;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest_MathParser
+{
+    [TestClass]
+    public class UnitTest_DoubleToken
+    {
+        [TestMethod]
+        public void TestMethod_NewDoubleToken_IntString()
+        {
+            string doubleString = "123";
+
+            var token = new DoubleToken(doubleString);
+
+            Assert.AreEqual(123D, token.Value);
+        }
+
+    }
+}
