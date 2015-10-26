fadfce2d1e3dba65e45f3e816e0958e1d8efdc95
Igor <urffin2006@gmail.com>
2015-10-26 07:49:26 +0300

    add empty lexer test

---
+++ MathParser/UnitTest_MathParser/UnitTest_Lexer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest_MathParser
+{
+    [TestClass]
+    public class UnitTest_Lexer
+    {
+        [TestMethod]
+        public void TestMethod_GetLexem()
+        {
+
+        }
+    }
+}
